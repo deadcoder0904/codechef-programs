8c7b681fe46bc623242dfa82e056c93baba7c025
Akshay Kadam <akshaykadam0904@gmail.com>
2016-08-11 23:30:41 +0530

    Adding script on Thu Aug 11 23:30:41 IST 2016

--- palindromic.c
+++ palindromic.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-#define ll long long
-ll reverseNumber(ll j){
-	ll temp=0;
-	while(j!=0){
-		temp = temp*10 + j%10;
-		j/=10;
-	}
-	return temp;
-}
+
 int main()
 {
-	ll j=0,t,s=0,a,b;
-	scanf("%lld",&t);
+	int j=0,t,s=0,a,b,p,temp;
+	scanf("%d",&t);
 	while(t){
-		scanf("%lld %lld",&a,&b);
+		scanf("%d%d",&a,&b);
 		s = 0;
-		for (j = a; j < b; ++j)
+		for (j = a; j <= b; ++j)
 		{
-			if(j == reverseNumber(j))
+			p=j;
+			temp=0;
+			while(p!=0){
+				temp = temp*10 + p%10;
+				p/=10;
+			}
+			if(j == temp)
 				s += j;
 		}
-		printf("%lld\n",s);	
-		t-=1;
+		printf("%d\n",s);	
+		t--;
 	}
 	return 0;
 }
--- palindromic.py
+++ palindromic.py
@@ -1,9 +1,9 @@
 test = int(input())
 while test:
 	a,b = raw_input().split()
-	for x in xrange(int(a),int(b)):
-		sum = 0
+	sum = 0
+	for x in xrange(int(a),int(b)+1):
 		if str(x) == str(x)[::-1]:
 			sum += x
-		print(sum)	
+	print(sum)	
 	test -= 1
