7e4de0a0ec00c9bafd77dad7154dd02987616cf5
Akshay Kadam <akshaykadam0904@gmail.com>
2016-08-24 13:39:07 +0530

    Adding script on Wed Aug 24 13:39:07 IST 2016

--- fit-squares-in-triangle.cpp
+++ fit-squares-in-triangle.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+int find(int b){
+	if(b <= 1) return 0;
+	return (b-2)/2 + find(b-2);
+}
+
 int main(int argc, char const *argv[])
 {
-	
+	int t;
+	cin>>t;
+	while(t--){
+		int b;
+		cin>>b;
+		cout<<find(b)<<endl;
+	}
 	return 0;
 }
