36c57bf49bff39813b2bb354a2bdf921ac3e9048
Akshay Kadam <akshaykadam0904@gmail.com>
2016-08-24 13:42:15 +0530

    Adding script on Wed Aug 24 13:42:15 IST 2016

--- valid-triangles.cpp
+++ valid-triangles.cpp
@@ -1,1 +1,16 @@
-valid-triangles.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t,x,y,z;
+	cin>>t;
+	while(t--){
+		cin>>x>>y>>z;
+		if(x != 180 && y != 180 && z != 180 && x+y+z == 180)
+			cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+	}
+	return 0;
+}
