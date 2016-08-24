48ce9ddb3c1117895d5d185f139dca4c63a87c03
Akshay Kadam <akshaykadam0904@gmail.com>
2016-08-24 17:45:13 +0530

    Adding script on Wed Aug 24 17:45:13 IST 2016

--- chef-and-two-strings.cpp
+++ chef-and-two-strings.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,7 +8,17 @@ int main(int argc, char const *argv[])
 	int t;
 	cin>>t;
 	while(t--){
-		
+		string s1,s2;
+		cin>>s1>>s2;
+		int min = 0,max = 0;
+		for (int i = 0; i < s1.length(); ++i)
+		{
+			if (s1.at(i) != '?' && s2.at(i) != '?' && s1.at(i) != s2.at(i))
+				min++;
+			if(s1.at(i) == '?' && s2.at(i) == '?' || s1.at(i) != s2.at(i))
+				max++;
+		}
+		cout<<min<<" "<<max<<endl;
 	}
 	return 0;
 }
