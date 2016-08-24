b3801c51edac9ae4a287897bda69e096b9dd1053
Akshay Kadam <akshaykadam0904@gmail.com>
2016-08-24 16:23:44 +0530

    Adding script on Wed Aug 24 16:23:44 IST 2016

--- piece-of-cake.cpp
+++ piece-of-cake.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,7 +8,25 @@ int main(int argc, char const *argv[])
 	int t;
 	cin>>t;
 	while(t--){
-		
+		int c[28]={0};
+		int i;int ans=0;
+		for(i=97;i<=122;i++)
+		c[i]=0;
+		string s;
+		cin>>s;
+		for(i=0;i<s.length();i++)
+		{
+			c[(int)(s[i])]++;
+			ans++;
+		}
+		int max=-1;
+		for(i=97;i<=122;i++)
+		{
+			if(c[i]>max)max=c[i];
+		}
+		if(max==ans-max) 
+			cout<<"YES"<<endl;	
+		else  cout<<"NO"<<endl;
 	}
 	return 0;
 }
