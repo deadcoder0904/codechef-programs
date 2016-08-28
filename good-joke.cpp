789fb9c866c455536c0870f10491f882580f9f87
Akshay Kadam <akshaykadam0904@gmail.com>
2016-08-28 14:46:08 +0530

    Adding script on Sun Aug 28 14:46:07 IST 2016

--- good-joke.cpp
+++ good-joke.cpp
@@ -7,7 +7,13 @@ int main(int argc, char const *argv[])
 	int t;
 	cin>>t;
 	while(t--){
-		
+		int N,ans=1,a,b;
+		cin>>N;
+		for (int i = 2; i <= N; ++i)
+			ans ^= i;
+		while(N--)
+			cin>>a>>b;
+		cout<<ans<<endl;
 	}
 	return 0;
 }
